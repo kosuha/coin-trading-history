453e458b9bee59f2be897184cf4f7994bf52942e
kosuha <okeydokekim@gmail.com>
2022-03-01 22:59:20 +0900

    node api server

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# good-game
+# coin-trading-bot의 매매 내역을 확인하는 웹
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql');
+const conn = require("./config/conn.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const db_info = {
+    host: conn.host,
+    port: conn.port,
+    user: conn.user,
+    password: conn.password,
+    database: conn.database
+};
+
+const db = mysql.createConnection(db_info);
+db.connect();
+
+app.get("/api/history", (req, res) => {
+	const query = "SELECT * FROM history";
+	db.query(query, (err, rows, fields) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).send({
+			success: true,
+			data: rows
+		})
+	});
+})
+
+app.listen(3000, () => console.log("### Start API Server ###"));
